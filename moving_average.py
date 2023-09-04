0d824063b0fbb5cb81a3a3754e95552f56c27bea
William-M-Fox <143985456+William-M-Fox@users.noreply.github.com>
2023-09-04 16:54:39 +1200

    Update moving_average.py

--- moving_average.py
+++ moving_average.py
@@ -1,12 +1,21 @@
 import pandas as pd
+import sys
 
+if len(sys.argv) != 4:
+    print("Usage: python3 calculate_sma.py <input_csv> <output_csv> <window_size>")
+    sys.exit(1)
 
-# Create a DataFrame from the sample data
-df = pd.read_csv("full_data.csv")
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+window_size = int(sys.argv[3])
 
-# Calculate a simple moving average with a window size of 3
-window_size = 90
+# Create a DataFrame from the input CSV file
+df = pd.read_csv(input_file)
+
+# Calculate a simple moving average with the specified window size
 df['Moving Average'] = df['Turnover'].rolling(window=window_size).mean().round(2)
 
-# Print the DataFrame with the SMA column
-df.to_csv("maggies_final.csv", index=False)
+# Save the DataFrame with the moving average column to the output CSV file
+df.to_csv(output_file, index=False)
+
+print(f"Simple Moving Average with a window size of {window_size} calculated and saved to {output_file}.")
